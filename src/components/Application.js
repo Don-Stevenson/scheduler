6bed13f3697b4bdaa1975ad150d3e3af451d9c8d
Don-Stevenson <dsteven1@gmail.com>
2020-01-21 18:13:00 -0500

    update state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import DayList from "components/DayList"
-import Appointment from "components/Appointment";
 import "components/Application.scss";
-const axios = require('axios')
+import Appointment from "components/Appointment";
+const axios = require('axios');
 
 const appointments = [
   {
@@ -60,12 +60,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("/api/days")
       .then((response) => {
+        console.log(response.data)
         setDays(response.data);
       })
   }, []);
@@ -87,11 +93,10 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            day={day}
-            setDay={setDay}
-          />
+        <DayList 
+        days={state.days} 
+        day={state.day} 
+        setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
