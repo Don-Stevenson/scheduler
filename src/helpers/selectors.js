49bf2379bbe0f200a9b8186ad1e1374783f781a3
Don-Stevenson <dsteven1@gmail.com>
2020-01-21 18:21:40 -0500

    add selector

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+    if (!state.days) {
+      return [];
+    }
+    if (state.days.length < 1) {
+      return [];
+    }
+    console.log(state.days);
+    let appointmentsForDay = state.days.find(e => e.name === day);
+    if (!appointmentsForDay) {
+      return [];
+    }
+    console.log("appointmentsForDay", appointmentsForDay);
+  
+    console.log("DAY OF WEEK: ", appointmentsForDay.name, "---- given appointments:", appointmentsForDay.appointments);
+    let apptMap = appointmentsForDay.appointments.map(apptID => apptID = state.appointments[apptID]);
+
+    console.log("APPT MAP_____", apptMap);
+    return (!appointmentsForDay || appointmentsForDay.appointments.length < 1) ? [] : appointmentsForDay.appointments.map(apptID => state.appointments[apptID]);
+  
+  } 
