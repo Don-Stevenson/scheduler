0542c58544db69d229ab19022c443bbb9c3ecbc1
Don-Stevenson <dsteven1@gmail.com>
2020-01-22 13:33:00 -0500

    add custom hook w test to manage visual mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace = false) => {
+    setHistory(prev =>
+      replace ? [...prev.slice(0, prev.length - 1), mode] : [...prev, mode]
+    );
+  }
+
+  const back = () => {
+    if (history.length < 2) return;
+    setHistory(prev => [...prev.slice(0, history.length - 1)]);
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
