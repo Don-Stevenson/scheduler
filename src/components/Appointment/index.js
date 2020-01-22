583d8f50078eea060a112f4c4985d9bd0d433c08
Don-Stevenson <dsteven1@gmail.com>
2020-01-22 16:18:07 -0500

    add empty, create and show using custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,35 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
+    const EMPTY = "EMPTY";
+    const SHOW = "SHOW";
+    const CREATE = "CREATE";
+
+    const { mode, transition, back } = useVisualMode(
+        props.interview ? SHOW : EMPTY
+    );
+
     return (
         <article className="appointment">
             <Header time={props.time} />
-            {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+            {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+            {mode === CREATE && (
+                <Form
+                    interviewers={[]}
+                    onCancel={() => back()}
+                />
+            )}
+            {mode === SHOW && (
+                <Show
+                    student={props.interview.student}
+                    interviewer={props.interview.interviewer}
+                />
+            )}
         </article>
+
     )
 }
