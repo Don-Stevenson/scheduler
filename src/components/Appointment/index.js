36f7802dda4fb5d2c3996bbc4f60d11ca0163a63
Don-Stevenson <dsteven1@gmail.com>
2020-01-22 19:27:50 -0500

    update to show interviewers properly

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,16 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 
 import useVisualMode from "hooks/useVisualMode";
-//import { getAppointmentsForDay, getInterview, getInterviewsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewsForDay } from "helpers/selectors";
 import Form from "components/Appointment/Form";
 
 
 //importing the other props
-
+import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Create from "components/Appointment/Form";
+import Edit from "components/Appointment/Form";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 
@@ -41,7 +44,7 @@ export default function Appointment(props) {
             {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
             {mode === CREATE && (
                 <Form
-                    interviewers={[props.interviewers]}
+                    interviewers={props.interviewers}
                     onCancel={() => back()}
                     onSave={save}
                 />
