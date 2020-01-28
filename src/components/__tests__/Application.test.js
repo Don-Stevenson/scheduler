db0805a7d29c67f932c3b49d69f09c7264905921
Don-Stevenson <dsteven1@gmail.com>
2020-01-27 20:33:33 -0500

    using async await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,11 +6,12 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
-}); 
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
